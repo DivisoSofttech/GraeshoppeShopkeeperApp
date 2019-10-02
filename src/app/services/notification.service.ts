c09dc6f1990f6d1b6bab3210b8e40974b44a564e
abilash-diviso <abilash.s@lxisoft.com>
2019-10-02 16:48:10 +0530

    added notification

--- src/app/services/notification.service.ts
+++ src/app/services/notification.service.ts
@@ -4,6 +4,7 @@ import {  Subscription } from 'rxjs';
 import { LocalNotifications } from '@ionic-native/local-notifications/ngx';
 import { Platform, NavController } from '@ionic/angular';
 import { NotificationDTO } from '../api/models';
+import { QueryResourceService } from '../api/services';
 
 @Injectable({
   providedIn: 'root'
@@ -17,7 +18,8 @@ export class NotificationService {
   constructor(private socket: Socket,
               private localNotifications: LocalNotifications,
               private platform: Platform,
-              private navCtrl: NavController) {
+              private navCtrl: NavController,
+              private queryResource: QueryResourceService) {
     this.socket.disconnect();
     this.onConnect().subscribe(data => {
       console.log('Socket has been connected successfully');
@@ -35,6 +37,7 @@ export class NotificationService {
   }
 
   subscribeToMyNotifications(user) {
+    this.getNoticationCount(user);
     console.log('Start listening to my notifications ', user);
     this.localNotifications.on('click').subscribe(event => {
       console.log('Notification clicked', event);
@@ -42,11 +45,10 @@ export class NotificationService {
     });
     this.localNotifications.on('accept').subscribe(event => {
       console.log('Accept clicked', event);
-      // this.navCtrl.navigateForward('restaurant');
+
     });
     this.localNotifications.on('reject').subscribe(event => {
       console.log('Reject clicked', event);
-      // this.navCtrl.navigateForward('restaurant');
     });
     return this.socket
          .fromEvent(user).subscribe((notification: NotificationDTO) => {
@@ -54,6 +56,7 @@ export class NotificationService {
            console.log('Notification count is ', this.notificationCount);
            console.log(notification);
            this.platform.ready().then(() => {
+             if (notification.type === 'Pending-Order') {
               this.localNotifications.schedule({
                 title: notification.title,
                 text: notification.message + '\nTracking ID is ' + notification.targetId,
@@ -67,8 +70,29 @@ export class NotificationService {
                   { id: 'reject',  title: 'Reject' }
               ]
               });
+             } else if (notification.type === 'Confirmed-Order') {
+              this.localNotifications.schedule({
+                title: notification.title,
+                text: notification.message + '\nTracking ID is ' + notification.targetId,
+                foreground: true,
+                wakeup: true,
+                lockscreen: true,
+                sound: 'file://assets/beep.mp3',
+                icon: 'file://assets/images/logo.png'
+              });
+             }
             });
         });
+  }
+
+
+ getNoticationCount(user) {
+      this.queryResource
+        .getNotificationCountByReceiveridAndStatusUsingGET({
+          status: 'unread',
+          receiverId: user
+        })
+        .subscribe(num => (this.notificationCount = num));
   }
 
 disconnectToMyNotifications() {
@@ -77,4 +101,5 @@ disconnectToMyNotifications() {
     this.notificationListenSubscription.unsubscribe();
     this.socket.disconnect();
   }
+
 }
